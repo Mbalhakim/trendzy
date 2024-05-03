400b0582f1d7a276964d2721fc76382e4a00c86b
Mbalhakim <mb.alhakim@gmail.com>
2024-05-03 12:03:12 +0200

    Fixed imports for assets

--- app/index.jsx
+++ app/index.jsx
@@ -1,25 +1,17 @@
-import { StatusBar, StyleSheet, Text, View } from 'react-native'
+import { StatusBar, Text, View } from 'react-native'
 import React from 'react'
 import { Link } from 'expo-router'
 
 const RootLayout = () => {
     return (
-        <View style={styles.container}>
-            <Text>index</Text>
+        <View className="flex-1 items-center justify-center bg-red-200">
+            <Text className="text-3xl">index</Text>
             <StatusBar style="auto" />
             <Link href="/profile" style={{ color: 'blue' }} >Go to Profile</Link>
         </View>
     )
 }
 
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backGroundColor: 'fff',
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
-});
 export default RootLayout
 
 
